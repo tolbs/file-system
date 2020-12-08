7a7c9903adedcdf6aae20b6f6d27e35d8b34833e
aavina22 <66487236+aavina22@users.noreply.github.com>
2020-12-07 23:51:50 -0800

    Update SpaceManager.c

--- SpaceManager.c
+++ SpaceManager.c
@@ -1,3 +1,17 @@
+/**************************************************************
+* Class:  CSC-415-03 Fall 2020
+* Name: (KAAT) Kimberly Nivon, Angela Avina Garcia, Abishek Neralla, Tolby Lam
+* Student ID: 916563133(Kimberly), 917579109(Angela), 917725840(Abishek) 
+*		920787020(Tolby)
+* Project: File System Project
+*
+* File: SpaceManager.c
+*
+* Description: space management file , freeing our blocks
+*
+**************************************************************/
+
+
 #include "VolumeCreator.h"
 #include "SpaceManager.h"
 
