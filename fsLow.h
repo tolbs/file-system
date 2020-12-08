15d0e1d4b9cf9a732db97c5ccc95125518bf373f
aavina22 <66487236+aavina22@users.noreply.github.com>
2020-12-07 23:59:08 -0800

    Update fsLow.h

--- fsLow.h
+++ fsLow.h
@@ -1,8 +1,9 @@
 /**************************************************************
-* Class:  CSC-415
-* Name: Professor Bierman
-* Student ID: N/A
-* Project: Basic File System
+* Class:  CSC-415-03 Fall 2020
+* Name: (KAAT) Kimberly Nivon, Angela Avina Garcia, Abishek Neralla, Tolby Lam
+* Student ID: 916563133(Kimberly), 917579109(Angela), 917725840(Abishek) 
+*		920787020(Tolby)
+* Project: File System Project
 *
 * File: fsLow.h
 *
